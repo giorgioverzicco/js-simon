7c939427e2a56e1e7a316123ab5cb13ab70104ea
giorgioverzicco <giorgioverzicco00@gmail.com>
2022-05-12 15:09:39 +0200

    add some ui, need to finish

--- js/app.js
+++ js/app.js
@@ -1,5 +1,50 @@
+// ============================
+// OLD CODE
+// ============================
+// const randomNumbers = [];
+
+// while (randomNumbers.length < 5) {
+//   const rand = Math.floor(Math.random() * 100) + 1;
+//   if (!randomNumbers.includes(rand)) {
+//     randomNumbers.push(rand);
+//   }
+// }
+
+// const numbers = randomNumbers.join(" - ");
+// alert(`Ricordati questi numeri: ${numbers}`);
+
+// const timer = setTimeout(() => {
+//   const userNumbers = [];
+//   const correctNumbers = [];
+
+//   while (userNumbers.length < 5) {
+//     const num = Number(prompt("Inserisci i numeri che ricordi, uno alla volta:"));
+//     if (!userNumbers.includes(num)) {
+//       userNumbers.push(num);
+
+//       if (randomNumbers.includes(num)) {
+//         correctNumbers.push(num);
+//       }
+//     }
+//   }
+
+//   const numbers = correctNumbers.join(" - ");
+//   alert(`Hai indovinato ${correctNumbers.length} numeri: ${numbers}`);
+// }, 3000);
+
+// ============================
+// NEW CODE
+// ============================
+
+const maxTime = 30;
 const randomNumbers = [];
 
+// DOM Elements
+const numbers = document.querySelector(".numbers-wrapper");
+const timer = document.querySelector(".timer");
+const userInput = document.querySelector(".user-input");
+
+// generate random numbers
 while (randomNumbers.length < 5) {
   const rand = Math.floor(Math.random() * 100) + 1;
   if (!randomNumbers.includes(rand)) {
@@ -7,24 +52,31 @@ while (randomNumbers.length < 5) {
   }
 }
 
-const numbers = randomNumbers.join(" - ");
-alert(`Ricordati questi numeri: ${numbers}`);
+// add numbers into the dom
+randomNumbers.forEach((rand) => {
+  const num = document.createElement("div");
+  num.classList.add("number");
+  num.innerHTML = rand;
 
-const timer = setTimeout(() => {
-  const userNumbers = [];
-  const correctNumbers = [];
+  numbers.append(num);
+});
 
-  while (userNumbers.length < 5) {
-    const num = Number(prompt("Inserisci i numeri che ricordi, uno alla volta:"));
-    if (!userNumbers.includes(num)) {
-      userNumbers.push(num);
+// start timer by Immediately-invoked Function Expression (IIFE)
+// https://www.geeksforgeeks.org/how-to-execute-setinterval-function-without-delay-for-the-first-time-in-javascript/
+let timerCount = maxTime;
+const timerId = setInterval(
+  (function startTimer() {
+    timer.innerHTML = timerCount--;
+    if (timerCount < 0) {
+      // hide number ui
+      timer.style.display = "none";
+      numbers.style.display = "none";
+      // show user input for guessing
+      userInput.style.display = "flex";
 
-      if (randomNumbers.includes(num)) {
-        correctNumbers.push(num);
-      }
+      clearInterval(timerId);
     }
-  }
-
-  const numbers = correctNumbers.join(" - ");
-  alert(`Hai indovinato ${correctNumbers.length} numeri: ${numbers}`);
-}, 3000);
+    return startTimer;
+  })(),
+  1000
+);
